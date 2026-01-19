ad1801c57ba4928db6c4db20b9ed2273ba21d4dd
almostlight <thealmostlight@gmail.com>
2026-01-19 19:46:38 +0100

    fixed icon bug

--- Linux/desktop_entry.sh
+++ Linux/desktop_entry.sh
@@ -2,9 +2,9 @@
 
 VM_NAME=$(sudo virsh list --all --name | grep -i "win")
 DIR="$(dirname $(realpath $0))"
-ICON_PATH="$DIR/logo.svg"
-TARGET="$HOME/.local/share/applications/looking-glass-$VM_NAME.desktop"
-LAUNCHER_SCRIPT="$HOME/.local/bin/launch_${VM_NAME}_lg.sh"
+TARGET="$HOME/.local/share/applications/looking-glass-${VM_NAME}.desktop"
+ICON_TARGET="$HOME/.local/share/icons/looking-glass-${VM_NAME}.svg"
+LAUNCHER_TARGET="$HOME/.local/bin/looking-glass-${VM_NAME}.sh"
 
 if [[ -f "$TARGET" ]]; then
     read -p "File '$TARGET' already exists. Overwrite? [y/N] " -n 1 -r
@@ -16,24 +16,28 @@ if [[ -f "$TARGET" ]]; then
 fi
 
 mkdir -p "$(dirname "$TARGET")"
-mkdir -p "$(dirname "$LAUNCHER_SCRIPT")"
+mkdir -p "$(dirname "$LAUNCHER_TARGET")"
+mkdir -p "$(dirname "$ICON_TARGET")"
+cp -f "$DIR/logo.svg" "$ICON_TARGET"
+cp -f "$DIR/launch_vm.sh" "$LAUNCHER_TARGET"
+# rsvg-convert -w 48 -h 48 "$ICON" -o "$ICON_TARGET.xmp"
+# chmod 777 "$ICON_TARGET.xmp"
 
+# black magic with rev: https://unix.stackexchange.com/a/617832
 echo "
 [Desktop Entry]
 Name=Windows 11 (Looking Glass)
 Comment=Launch Windows 11 VM with Looking Glass (auto-shutdown)
-Icon=$ICON_PATH
+Icon=$(realpath $ICON_TARGET |rev| cut -d"." -f2- |rev)
 Terminal=false
 Type=Application
 Categories=System;Emulator;
 StartupNotify=true
-Exec=$LAUNCHER_SCRIPT
+Exec=$LAUNCHER_TARGET
 " | tee $TARGET > /dev/null 2>&1
 
-cp "$DIR/launch_vm.sh" $LAUNCHER_SCRIPT
-
-sudo chmod +x "$TARGET"
-chmod +x "$LAUNCHER_SCRIPT"
+chmod +x "$TARGET"
+chmod +x "$LAUNCHER_TARGET"
 
 # Passwordless sudo configuration for virsh (optional)
 echo "To allow passwordless VM start/shutdown, run:"
